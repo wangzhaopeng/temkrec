eb91cc3d72908bb92b26cd3d703d0af260f1a097
wzp <wzhaopeng@foxmail.com>
2018-03-16 03:39:19 -0700

    一些整理

--- video/vpucls.cpp
+++ video/vpucls.cpp
@@ -38,15 +38,7 @@ public:
 class Encoder
 {
 public:
-	EncHandle m_nHandle;
-	unsigned long m_nPhyAddr;
-	unsigned long m_nVirtualAddr;
-	int m_nYUV420Widht;      
-	int m_nYUV420Height;      
-	FrameBuffer *m_pFrameBuffer;
-	framebuf **m_pFrameBufferPool;
 	ExtBufCfg m_struExtBufCfg;
-	int m_ndstfg;
 };
 
 
@@ -91,57 +83,6 @@ static void FreeFrameBuf(framebuf *fb){
 //cout<<"FreeFrameBuf"<<endl;
 }
 
-////return -1 err    0 ok
-static int AllocEncoderFrameBuffer(Encoder *pEncoder){
-	EncHandle handle = pEncoder->m_nHandle;
-	FrameBuffer *fb;
-	framebuf **pfbpool;
-	unsigned long subSampBaseA = 0, subSampBaseB = 0;
-	EncExtBufInfo extbufinfo = {0};
-	RetCode ret;
-	fb = pEncoder->m_pFrameBuffer = (FrameBuffer*)calloc(5, sizeof(FrameBuffer));
-	if (fb == NULL) 
-		return -1;
-
-	pfbpool = pEncoder->m_pFrameBufferPool = (framebuf**)calloc(5,sizeof(struct frame_buf *));
-	if (pfbpool == NULL) 
-		return -1;
-
-	for (int i = 0; i < 4+1; i++){
-		pfbpool[i] = AllocFrameBuf(pEncoder->m_nYUV420Widht, pEncoder->m_nYUV420Height);
-		if (pfbpool[i] == NULL)
-			return -1;
-	}
-	for (int i = 0; i < 4+1; i++){
-		fb[i].myIndex = i;
-		fb[i].bufY = pfbpool[i]->addrY;
-		fb[i].bufCb = pfbpool[i]->addrCb;
-		fb[i].bufCr = pfbpool[i]->addrCr;
-		fb[i].strideY = pfbpool[i]->strideY;
-		fb[i].strideC = pfbpool[i]->strideC;
-	}
-	subSampBaseA = fb[2].bufY;
-	subSampBaseB = fb[3].bufY;
-	extbufinfo.scratchBuf = pEncoder->m_struExtBufCfg;
-	ret = vpu_EncRegisterFrameBuffer(handle, fb, 2, pEncoder->m_nYUV420Widht, pEncoder->m_nYUV420Widht,
-		subSampBaseA, subSampBaseB, &extbufinfo);
-	if (ret != RETCODE_SUCCESS) 
-		return -1;
-
-	return 0;
-}
-
-static void FreeEncoderFrameBuffer(Encoder *pEncoder)
-{
-	for (int i = 0; i<4+1; i++) {
-		FreeFrameBuf(pEncoder->m_pFrameBufferPool[i]);
-	}
-	free(pEncoder->m_pFrameBuffer);
-	free(pEncoder->m_pFrameBufferPool);
-}
-
-
-
 class vpucls
 {
 public:
@@ -153,6 +94,7 @@ public:
 	int get_pps(vector<unsigned char>&sps);
 	int enc(const char* pYUV420Data,int len,bool bIFrame,char* pH264Data,int &H264Datalen);
 
+private:
 	int m_w,m_h,m_f;
 
 	vector<unsigned char>m_sps,m_pps;
@@ -160,7 +102,17 @@ public:
 	void*m_enc;
 	void*mp_vpu_mem_desc;
 	void*mp_EncHandle;
+	void **m_pFrameBufferPool;
+	void *m_pFrameBuffer;
+
+	unsigned long m_phy_addr;
+	unsigned long m_virt_addr;
+	void FreeEncoderFrameBuffer(Encoder *pEncoder);
+	int AllocEncoderFrameBuffer(Encoder *pEncoder);
 };
+
+
+
 
 vpucls::vpucls(const int w,const int h,const int f)
 {
@@ -179,27 +131,33 @@ void vpucls::de_init(void)
 	Encoder* pEncoder = (Encoder*)m_enc;
 	if(pEncoder){
 		FreeEncoderFrameBuffer(pEncoder);
-		RetCode ret = vpu_EncClose(pEncoder->m_nHandle);
-		if (ret == RETCODE_FRAME_NOT_COMPLETE){
-			vpu_SWReset(pEncoder->m_nHandle, 0);
-			vpu_EncClose(pEncoder->m_nHandle);
-		}
 
 		free(pEncoder);
 		m_enc =NULL;
 	}
 
 
+	if(m_virt_addr){
+		IOFreeVirtMem((vpu_mem_desc *)mp_vpu_mem_desc);
+	}
+
+	EncHandle *p_EncHandle = (EncHandle *)mp_EncHandle;
+	if(p_EncHandle){
+		RetCode ret = vpu_EncClose(*p_EncHandle);
+		if (ret == RETCODE_FRAME_NOT_COMPLETE){
+			vpu_SWReset(*p_EncHandle, 0);
+			vpu_EncClose(*p_EncHandle);
+		}
+	}
+
+	if(mp_vpu_mem_desc){
+		IOFreePhyMem((vpu_mem_desc *)mp_vpu_mem_desc);
+		delete (vpu_mem_desc *)mp_vpu_mem_desc;mp_vpu_mem_desc = NULL;
+	}
 
 	vector<unsigned char>vtem;
 	m_sps=m_pps=vtem;
 
-	vpu_mem_desc *p_mem_desc = (vpu_mem_desc *)mp_vpu_mem_desc;
-	if(p_mem_desc){
-		IOFreeVirtMem(p_mem_desc);
-		IOFreePhyMem(p_mem_desc);
-		delete p_mem_desc;p_mem_desc = NULL;
-	}
 //vpu_UnInit();
 }
 
@@ -211,6 +169,7 @@ int vpucls::init(void){
 	m_enc = NULL;
 	mp_vpu_mem_desc = NULL;
 	mp_EncHandle = NULL;
+	m_virt_addr = 0;
 
 	mp_vpu_mem_desc = new vpu_mem_desc;
 	vpu_mem_desc *p_mem_desc = (vpu_mem_desc *)mp_vpu_mem_desc;
@@ -222,8 +181,10 @@ int vpucls::init(void){
 		delete p_mem_desc;p_mem_desc = NULL;
 		return -1;
 	}
+	m_phy_addr = p_mem_desc->phy_addr;
+
 	EncOpenParam encop = {0};
-	encop.bitstreamBuffer = p_mem_desc->phy_addr;
+	encop.bitstreamBuffer = m_phy_addr;
 	encop.bitstreamBufferSize = STREAM_BUF_SIZE;
 	encop.bitstreamFormat = STD_AVC;
 	encop.mapType = 0;
@@ -295,11 +256,8 @@ int vpucls::init(void){
 		cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<" Encoder *pEncoder = new Encoder; err"<<endl;
 		return -1;
 	}
-	pEncoder->m_nVirtualAddr = IOGetVirtMem(p_mem_desc);
-	pEncoder->m_nPhyAddr = p_mem_desc->phy_addr;
-	pEncoder->m_nYUV420Widht = m_w;
-	pEncoder->m_nYUV420Height = m_h;
-	pEncoder->m_nHandle = *p_EncHandle;
+	m_virt_addr = IOGetVirtMem(p_mem_desc);
+
 	int nRet = AllocEncoderFrameBuffer(pEncoder);
 	if(nRet<0){
 		cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<" AllocEncoderFrameBuffer err"<<endl;
@@ -309,19 +267,69 @@ int vpucls::init(void){
 	return 0;
 }
 
+void vpucls::FreeEncoderFrameBuffer(Encoder *pEncoder)
+{
+	for (int i = 0; i<4+1; i++) {
+		FreeFrameBuf((framebuf *)m_pFrameBufferPool[i]);//(pEncoder->m_pFrameBufferPool[i]);
+	}
+	free(m_pFrameBuffer);
+	free(m_pFrameBufferPool);
+}
+
+
+
+////return -1 err    0 ok
+int vpucls::AllocEncoderFrameBuffer(Encoder *pEncoder){
+	EncHandle handle = *(EncHandle*)mp_EncHandle;
+	FrameBuffer *fb;
+	framebuf **pfbpool;
+	unsigned long subSampBaseA = 0, subSampBaseB = 0;
+	EncExtBufInfo extbufinfo = {0};
+	RetCode ret;
+	m_pFrameBuffer = (FrameBuffer*)calloc(5, sizeof(FrameBuffer));
+	fb = (FrameBuffer*)m_pFrameBuffer;
+	if (fb == NULL) 
+		return -1;
+
+	m_pFrameBufferPool = (void**)calloc(5,sizeof(struct frame_buf *));
+	pfbpool = (framebuf**)m_pFrameBufferPool;
+	if (pfbpool == NULL) 
+		return -1;
+
+	for (int i = 0; i < 4+1; i++){
+		pfbpool[i] = AllocFrameBuf(m_w, m_h);
+		if (pfbpool[i] == NULL)
+			return -1;
+	}
+	for (int i = 0; i < 4+1; i++){
+		fb[i].myIndex = i;
+		fb[i].bufY = pfbpool[i]->addrY;
+		fb[i].bufCb = pfbpool[i]->addrCb;
+		fb[i].bufCr = pfbpool[i]->addrCr;
+		fb[i].strideY = pfbpool[i]->strideY;
+		fb[i].strideC = pfbpool[i]->strideC;
+	}
+	subSampBaseA = fb[2].bufY;
+	subSampBaseB = fb[3].bufY;
+	extbufinfo.scratchBuf = pEncoder->m_struExtBufCfg;
+	ret = vpu_EncRegisterFrameBuffer(handle, fb, 2, m_w, m_w,
+		subSampBaseA, subSampBaseB, &extbufinfo);
+	if (ret != RETCODE_SUCCESS) 
+		return -1;
+
+	return 0;
+}
+
+
 ////return -1 err    0 ok
 int vpucls::get_sps(vector<unsigned char>&sps){
-	Encoder* pEncoder = (Encoder*)m_enc;
-	if(pEncoder == NULL)
-		return -1;
 	if(m_sps.size()>0){
 		sps = m_sps;
 		return 0;
 	}
 	EncHeaderParam enchdr_param = {0};
-	EncHandle handle = pEncoder->m_nHandle;
 	enchdr_param.headerType = SPS_RBSP;
-	RetCode ret = vpu_EncGiveCommand(handle, ENC_PUT_AVC_HEADER, &enchdr_param);
+	RetCode ret = vpu_EncGiveCommand(*(EncHandle*)mp_EncHandle, ENC_PUT_AVC_HEADER, &enchdr_param);
 	if(ret != RETCODE_SUCCESS){
 		cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<" vpu_EncGiveCommand err"<<endl;
 		return -1;
@@ -329,7 +337,7 @@ int vpucls::get_sps(vector<unsigned char>&sps){
 
 	vector<unsigned char>v_temsps(enchdr_param.size);
 
-	unsigned long nVirtualaddr = pEncoder->m_nVirtualAddr + (enchdr_param.buf - pEncoder->m_nPhyAddr);
+	unsigned long nVirtualaddr = m_virt_addr + (enchdr_param.buf - m_phy_addr);
 	memcpy(&v_temsps[0],(char*)nVirtualaddr,enchdr_param.size);
 	sps=m_sps=v_temsps;
 	return 0;
@@ -337,39 +345,33 @@ int vpucls::get_sps(vector<unsigned char>&sps){
 
 ////return -1 err    0 ok
 int vpucls::get_pps(vector<unsigned char>&pps){
-	Encoder* pEncoder = (Encoder*)m_enc;
-	if(pEncoder == NULL)
-		return -1;
 	if(m_pps.size()>0){
 		pps = m_pps;
 		return 0;
 	}
 	EncHeaderParam enchdr_param = {0};
-	EncHandle handle = pEncoder->m_nHandle;
 	enchdr_param.headerType = PPS_RBSP;
-	RetCode ret = vpu_EncGiveCommand(handle, ENC_PUT_AVC_HEADER, &enchdr_param);
+	RetCode ret = vpu_EncGiveCommand(*(EncHandle*)mp_EncHandle, ENC_PUT_AVC_HEADER, &enchdr_param);
 	if(ret != RETCODE_SUCCESS){
 		cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<" vpu_EncGiveCommand err"<<endl;
 		return -1;
 	}
+
 	vector<unsigned char>v_tempps(enchdr_param.size);
-	unsigned long nVirtualaddr = pEncoder->m_nVirtualAddr + (enchdr_param.buf - pEncoder->m_nPhyAddr);
+	unsigned long nVirtualaddr = m_virt_addr + (enchdr_param.buf - m_phy_addr);
 	memcpy(&v_tempps[0],(char*)nVirtualaddr,enchdr_param.size);
 	pps=m_pps=v_tempps;
 	return 0;
 }
 
 ////return -1 err    0 ok
-int vpucls::enc(const char* pYUV420Data,int len,bool bIFrame,char* pH264Data,int &H264Datalen)
-{
-	Encoder* pEncoder = (Encoder*)m_enc;
-	if(pEncoder == NULL)
-		return -1;
-	int nImageLen = pEncoder->m_nYUV420Widht * pEncoder->m_nYUV420Height * 3 / 2;
+int vpucls::enc(const char* pYUV420Data,int len,bool bIFrame,char* pH264Data,int &H264Datalen){
+	int nImageLen = m_w * m_h * 3 / 2;
 	if(pYUV420Data == NULL || len != nImageLen)
 		return -1;
 	EncParam  enc_param = {0};
-	enc_param.sourceFrame = &pEncoder->m_pFrameBuffer[4];
+	FrameBuffer * p_framebuffer = (FrameBuffer *)m_pFrameBuffer;
+	enc_param.sourceFrame = &p_framebuffer[4];
 	enc_param.quantParam = 23;
 	if(bIFrame)
 		enc_param.forceIPicture = 1;
@@ -379,10 +381,10 @@ int vpucls::enc(const char* pYUV420Data,int len,bool bIFrame,char* pH264Data,int
 	enc_param.enableAutoSkip = 1;
 	enc_param.encLeftOffset = 0;
 	enc_param.encTopOffset = 0;
-	framebuf *pfb = pEncoder->m_pFrameBufferPool[4];
+	framebuf *pfb = (framebuf *)m_pFrameBufferPool[4];
 	unsigned long y_addr = pfb->addrY + pfb->desc.virt_uaddr - pfb->desc.phy_addr;
 	memcpy((char*)y_addr,pYUV420Data,len);
-	RetCode ret = vpu_EncStartOneFrame(pEncoder->m_nHandle, &enc_param);
+	RetCode ret = vpu_EncStartOneFrame(*(EncHandle*)mp_EncHandle, &enc_param);
 	if ( ret != RETCODE_SUCCESS){ 
 		cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<" vpu_EncStartOneFrame err"<<" ret: "<<ret<<endl;
 		return -1;
@@ -395,19 +397,20 @@ int vpucls::enc(const char* pYUV420Data,int len,bool bIFrame,char* pH264Data,int
 		{
 			cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<" vpu_WaitForInt(200)*20"<<endl;
 			cout << "err vpu_SWReset "<<endl;
-			ret = vpu_SWReset(pEncoder->m_nHandle, 0);
+			ret = vpu_SWReset(*(EncHandle*)mp_EncHandle, 0);
 			return -1;
 		}
 		nLoop++;
 	}
 	EncOutputInfo outinfo = {0};
-	ret = vpu_EncGetOutputInfo(pEncoder->m_nHandle, &outinfo);
+	ret = vpu_EncGetOutputInfo(*(EncHandle*)mp_EncHandle, &outinfo);
 	if (ret != RETCODE_SUCCESS){
 		cerr<<__FILE__<<" "<<__FUNCTION__<<" "<<__LINE__<<endl;
 		cout << "err vpu_EncGetOutputInfo "<< " ret != RETCODE_SUCCESS "<<"ret "<<ret<<endl;
 		return -1;
 	}
-	unsigned long nVirtualBufferAddr = pEncoder->m_nVirtualAddr + (outinfo.bitstreamBuffer - pEncoder->m_nPhyAddr);
+
+	unsigned long nVirtualBufferAddr = m_virt_addr + (outinfo.bitstreamBuffer - m_phy_addr);
 	memcpy(pH264Data,(char*)nVirtualBufferAddr,outinfo.bitstreamSize);
 	H264Datalen = outinfo.bitstreamSize;
 	return 0;
@@ -449,7 +452,7 @@ sleep(20);
 
 	time_t sec = time(NULL);
 	cout <<ctime(&sec);
-	ofstream outh264("vpufffde250.264", ios::out | ios::binary);
+	ofstream outh264("vpu0.264", ios::out | ios::binary);
 	outh264.write((char*)&v_sps[0], v_sps.size()); outh264.write((char*)&v_pps[0], v_pps.size());
 
 	for(int sec = 0; sec < 10; sec++){
@@ -484,7 +487,7 @@ sleep(20);
 
 	time_t sec = time(NULL);
 	cout <<ctime(&sec);
-	ofstream outh264("vpufffde250-.264", ios::out | ios::binary);
+	ofstream outh264("vpu1.264", ios::out | ios::binary);
 	outh264.write((char*)&v_sps[0], v_sps.size()); outh264.write((char*)&v_pps[0], v_pps.size());
 
 	for(int sec = 0; sec < 10; sec++){
